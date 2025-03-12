9347d67a95a1e253ef4d76f79e0a9293e6c18839
Filip Šedivý <sedivy@apparently.cz>
2025-03-12 18:55:04 +0100

    interface pro příchozí data, button komponent

---
+++ client/src/components/button.tsx
@@ -0,0 +1,38 @@
+import { FC, ReactNode } from "react"
+
+export enum ButtonStyle {
+    RED,
+    BLUE
+}
+
+interface ButtonProps {
+    children: ReactNode
+    onClick: () => void
+    style: ButtonStyle
+}
+
+
+const Button: FC<ButtonProps> = ({ children, onClick, style }) => {
+
+    function getButtonStyle(style: ButtonStyle) {
+        switch (style) {
+            case ButtonStyle.RED:
+                return "bg-red-500 hover:bg-red-400 active:bg-red-600";
+            case ButtonStyle.BLUE:
+                return "bg-blue-500 hover:bg-blue-400 active:bg-blue-600";
+            default:
+                return "bg-gray-500";
+        }
+    };
+
+    return (
+        <button
+            className={`size-[32px] rounded-[99px] ${getButtonStyle(style)} flex items-center justify-center`}
+            onClick={onClick}
+        >
+            <div className="text-white">{children}</div>
+        </button>
+    )
+};
+
+export default Button;
---
+++ client/src/types.ts
@@ -0,0 +1,48 @@
+export interface Character {
+    ID: string;
+    Name: string;
+    Gender: string;
+    Ability: string;
+    "Minimal distance": string;
+    Weight: string;
+    Born: string;
+    "In space since": string;
+    "Beer consumption (l/y)": string;
+    "Knows the answer?": string;
+}
+
+export interface Nemesis {
+    ID: string;
+    "Character ID": string;
+    "Is alive?": string;
+    Years: string;
+}
+
+export interface Secrete {
+    ID: string;
+    "Nemesis ID": string;
+    "Secrete Code": string;
+}
+
+export interface Children {
+    has_nemesis?: {
+        records: {
+            data: Nemesis;
+            children: {
+                has_secrete?: {
+                    records: {
+                        data: Secrete;
+                        children: {};
+                    }[];
+                };
+            };
+        }[];
+    };
+}
+
+export interface CharacterRecord {
+    data: Character;
+    children: Children;
+}
+
+export type CharacterData = CharacterRecord[];
